6a0d49cef4fd24c917bc898bc8d20bfa19fa54d7
Vicky <twiks@Vickys-MacBook-Pro.local>
2014-03-10 17:31:04 +0700

    Added carousel draft

--- js/main.js
+++ js/main.js
@@ -5,3 +5,47 @@
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
 });
+
+$(document).on('ready', function(){
+    $('.image-preview').fancybox();
+});
+
+$(document).on('ready', function(){
+    $(function() {
+
+        $('.carousel_feedback .carousel__viewport').jcarousel({
+            wrap: 'circular'
+        });
+
+        $('.carousel_feedback .carousel__arrow_prev').jcarouselControl({
+            // Options go here
+            target: '-=1'
+        });
+
+        $('.carousel_feedback .carousel__arrow_next').jcarouselControl({
+            // Options go here
+            target: '+=1'
+        });
+
+    });
+});
+
+$(document).on('ready', function(){
+    $(function() {
+
+        $('.carousel_project').jcarousel({
+            wrap: 'circular'
+        });
+
+        $('.carousel_project .carousel__arrow_prev').jcarouselControl({
+            // Options go here
+            target: '-=1'
+        });
+
+        $('.carousel_project .carousel__arrow_next').jcarouselControl({
+            // Options go here
+            target: '+=1'
+        });
+
+    });
+});
